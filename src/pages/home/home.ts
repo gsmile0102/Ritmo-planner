6dff5c0f2b80031cfc4b2cf7ca45812f7224e704
gsmile0102 <gsmile0102@gmail.com>
2018-01-30 16:53:12 +0800

    reinstall dependencies

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,59 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  eventSource = [];
+  viewTitle: string;
+  isToday: boolean;
+  selectedDay = new Date();
 
+  calendar = {
+      mode: 'month',
+      currentDate: new Date()
+  };
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private alertCtrl: AlertController) {
 
+  }
+
+  changeMode(mode) {
+    this.calendar.mode = mode;
+  }
+
+  today() {
+    this.calendar.currentDate = new Date();
+  }
+
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  onEventSelected(event) {
+    let start = moment(event.startTime).format('LLLL');
+    let end = moment(event.endTime).format('LLLL');
+
+    let alert = this.alertCtrl.create({
+      title: '' + event.title,
+      subTitle: 'From: ' + start + '<br>To: ' + end,
+      buttons: ['OK']
+    })
+    alert.present();
+  }
+
+  onTimeSelected(ev) {
+    this.selectedDay = ev.selectedTime;
+  }
+
+  onCurrentDateChanged(event: Date){
+    var today = new Date();
+    today.setHours(0,0,0,0);
+    event.setHours(0,0,0,0);
+    this.isToday = today.getTime() === event.getTime();
   }
 
 }
